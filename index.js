cbe2d228461114a27298c8aa79f13b66491f4ee0
Mdkkdjssnsb <163823860+Mdkkdjssnsb@users.noreply.github.com>
2024-06-10 18:12:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,22 @@ app.get('/', (req, res) => {
 // Array to store request timestamps
 const requestTimestamps = [];
 
+app.get("/api/wallpaper", async (req, res) => {
+   const { query } = req.query;
+
+    if (!query) {
+        return res.status(400).json({ error: 'Please provide a query' });
+    }
+
+  try {
+   const response = await axios.get(`https://pixabay.com/api?query ${query}&image_type=photo&per_page=20&key=39178311-acadeb32d7e369897e41dba06&q=`);
+
+   res.json(response.data);
+ } catch (error) {
+   res.status(500).json({ error: error.message });
+ }
+});
+
 app.get("/api/prodia", async (req, res) => {
  try {
    const { prompt } = req.query.prompt;
@@ -30,7 +46,7 @@ app.get("/api/prodia", async (req, res) => {
 
    const response = await axios.get(`https://itsaryan.onrender.com/g?prompt=${encodeURIComponent(promot)}&model={model}`);
 
-   res.json({ response.data });
+   res.json(response.data);
  } catch (error) {
    res.status(500).json({ error: error.message });
  }
