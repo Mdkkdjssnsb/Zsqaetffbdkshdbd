625f7c1a097b7b71fdf5a782dd644c767a8092a8
Mdkkdjssnsb <163823860+Mdkkdjssnsb@users.noreply.github.com>
2024-06-22 10:32:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
+const fs = require('fs');
+const axios = require('axios');
+const express = require('express');
+const app = express();
 
+app.get("/api/wallpaper", async (req, res) => {
+   const { query } = req.query;
+
+    if (!query) {
+        return res.status(400).json({ error: 'Please provide a query' });
+    }
+
+  try {
+   const response = await axios.get(`https://pixabay.com/api?query ${query}&image_type=photo&per_page=20&key=39178311-acadeb32d7e369897e41dba06&q=`);
+
+   res.json(response.data);
+ } catch (error) {
+   res.status(500).json({ error: error.message });
+ }
+});
+
+app.get('/api/bing', async (req, res) => {
+  const { prompt } = req.query; // Change 'prompt' to 'q'
+
+  try {
+    const response = await axios.get(`https://bing-xxx.onrender.com/q`, {
+      params: {
+        prompt: encodeURIComponent(prompt) // encodeURIComponent the query parameter
+      }
+    });
+
+    const answer = response.data;
+    res.json(answer);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/api/shoti', async (req, res) => {
+try {
+    const response = await axios.get(`https://shoti-srv2-itslibyzxy0.koyeb.app/api/v1/request-f`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+   
